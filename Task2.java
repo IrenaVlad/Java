a52dc16450e06b2c76243d03c165828f27589126
IrenaVlad <iriscka@bk.ru>
2022-12-15 14:08:03 +0100

    change Task2

--- Task2.java
+++ Task2.java
@@ -2,11 +2,10 @@ import java.util.Scanner;
 
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(new Random().nextInt(10)).length());
-        /*Scanner iScanner = new Scanner(System.in);
+       Scanner iScanner = new Scanner(System.in);
        System.out.printf("Введите число ");
        int a = iScanner.nextInt();
-       System.out.println(Integer.toBinaryString(a.length()));
-       iScanner.close(); */
+       System.out.println(Integer.toBinaryString(a).length());
+       iScanner.close();
     }
 }    
