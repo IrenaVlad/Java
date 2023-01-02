afe07e5571f0ffaca8587259b6c2fe542023e02e
IrenaVlad <iriscka@bk.ru>
2023-01-02 14:42:01 +0100

    add homework4

--- Homework4.java
+++ Homework4.java
@@ -1,72 +1,129 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Homework4 {
-    static ArrayList<String> f_name = new ArrayList<>();
-    static ArrayList<String> i_name = new ArrayList<>();
-    static ArrayList<String> o_name = new ArrayList<>();
-    static ArrayList<Integer> age = new ArrayList<>();
-    static ArrayList<Boolean> gender = new ArrayList<>();
-    static List<Integer> linkedList = new LinkedList<>();
+    static List<List<String>> mainList = new ArrayList<>();
+    final static Integer FIO_INDEX = 0;
+    final static Integer AGE_INDEX = 1;
+    final static Integer GENDER_INDEX = 2;
+    final static String GENDER_MALE = "М";
+    final static String GENDER_FEMALE = "Ж";
+    final static String PRINT_TEMPLATE = "%s. %s возраст: %s, пол: %s";
 
     public static void main(String[] args) {
-        putTo("Ivanov Ivan Ivanovich", 16, "M");
-        putTo("Ivanov Fedr Ivanovich", 15, "M");
-        putTo("Ivanova Anna Ivanovna", 25, "F");
-        putTo("Ivanov Stepan Ivanovich", 51, "M");
-        putTo("Ivanova Ivanka Ivanovna", 35, "F");
-        putTo("Ivanov Semen Ivanovich", 21, "M");
 
-        for (int i =0; i <linkedList.size(); i++) 
-        {
-            if (gender.get(linkedList.get(i))) 
-            {
-                int tmpI = linkedList.get(i);
-                linkedList.remove(i);
-                linkedList.add(0, tmpI);
+        Scanner sc = new Scanner(System.in);
+        do {
+            String fio = getFio(sc);
+            String age = getAge(sc);
+            String gender = getGender(sc);
+            List<String> smallList = new ArrayList<>();
+            smallList.add(fio);
+            smallList.add(age);
+            smallList.add(gender);
+            mainList.add(smallList);
+
+        } while (!shouldStop(sc));
+
+        System.out.println("Сортировка по ФИО: ");
+        sortBySurnameAndPrint();
+        System.out.println();
+
+        System.out.println("Сортировка по возрасту: ");
+        sortByAgeAndPrint();
+        System.out.println();
+
+        System.out.println("Сортировка по полу: ");
+        sortByGenderAndPrint();
+        System.out.println();
+    }
+
+    private static boolean shouldStop(Scanner sc) {
+            System.out.println("Введите \"stop\" для окончания ввода данных или любое другое ЗНАЧЕНИЕ для продолжения: ");
+            String cont = sc.nextLine();
+        return cont.equalsIgnoreCase("stop");
+    }
+
+    private static String getGender(Scanner sc) {
+        while (true) {
+            System.out.println("Введите пол (m/f): ");
+            String gender = sc.nextLine();
+            if (gender.equalsIgnoreCase(GENDER_MALE) || gender.equalsIgnoreCase(GENDER_FEMALE)) {
+                return gender;
             }
+            System.out.println("Неверное значение, повторите ввод");
         }
-        int cnt = linkedList.size()-1;
-        while (cnt > -1)
-        {
-            int max_age = age.get(linkedList.get(cnt));
-            int index = cnt;
-            for (int i = 0; i < cnt; i++)
-            {
-                if (max_age < age.get(linkedList.get(i)))
-                {
-                    max_age = age.get(linkedList.get(i));
-                    index = i;
+    }
+
+    private static String getAge(Scanner sc) {
+        while (true) {
+            System.out.println("Введите возраст (0-100): ");
+            String age = sc.nextLine();
+            try {
+                int intAge = Integer.parseInt(age);
+                if (intAge < 0 || intAge > 100) {
+                    throw new NumberFormatException();
                 }
+                return age;
             }
-        int tmp = linkedList.get(cnt);
-        linkedList.set(cnt, linkedList.get(index));
-        linkedList.set(index, cnt);
-        cnt--;
+            catch (NumberFormatException exception) {
+                System.out.println("Неверное значение, повторите ввод");
+            }
         }
-        gender.retainAll(linkedList);
-        age.removeAll(linkedList);
-
-        linkedList.forEach(i -> System.out.println(f_name.get(i)+i_name.get(i)+o_name.get(i)+age.get(i)+gender.get(i)));
-    }
-    static void putTo(String fio, int ages, String gend)
-    {
-        age.get(ages);
-        String[] fio1 = fio.split(" ");
-        f_name.add(fio1[1]);
-        i_name.add(fio1[0]);
-        o_name.add(fio1[2]);
-        if (gend.contains("F"))
-        {
-            gender.add(true);
-        }
-        else 
-        {
-            gender.add(false);
-        }
-        linkedList.add(age.size()-1);
     }
 
+    private static String getFio(Scanner sc) {
+        while (true){
+            System.out.println("Введите ФИО: ");
+            String fio = sc.nextLine();
+            if (fio.length() > 0) {
+                return fio;
+            }
+            System.out.println("Длинна ФИО должна быть больше нуля, повторите ввод");
+        }
+    }
+
+    static void sortByAgeAndPrint() {
+        mainList.sort(new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> o1, List<String> o2) {
+                return Integer.parseInt(o1.get(AGE_INDEX))-Integer.parseInt(o2.get(AGE_INDEX));
+            }
+        });
+
+        printList();
+    }
+
+    static void sortByGenderAndPrint() {
+        mainList.sort(new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> o1, List<String> o2) {
+                return o1.get(GENDER_INDEX).compareTo(o2.get(GENDER_INDEX));
+            }
+        });
+
+        printList();
+    }
+
+    static void sortBySurnameAndPrint() {
+        mainList.sort(new Comparator<List<String>>() {
+            @Override
+            public int compare(List<String> o1, List<String> o2) {
+                return o1.get(FIO_INDEX).compareTo(o2.get(FIO_INDEX));
+            }
+        });
+        printList();
+    }
+
+    static void printList() {
+        for (int i = 0; i < mainList.size(); i++) {
+            System.out.printf((PRINT_TEMPLATE) + "%n",
+                    i + 1,
+                    mainList.get(i).get(FIO_INDEX),
+                    mainList.get(i).get(AGE_INDEX),
+                    mainList.get(i).get(GENDER_INDEX));
+        }
+    }
 }
---
+++ Homework5.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Homework5 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> fio = new HashMap<>();
+
+        fio.put(0, "Иванов Иван Иванович 45 м");
+        fio.put(1, "Петров Петр Петрович 15 м");
+        fio.put(2, "Сидоров Сидор Сидорович 35 м");
+        fio.put(3, "Иванова Анна Петровна 27 ж");
+        fio.put(4, "Карпова Луиза Андреевна 15 ж");
+
+        Set<Integer> keys = fio.keySet();
+        for (Integer key: keys) {
+            String temp1 = fio.
+        }
+
+    }
+}
